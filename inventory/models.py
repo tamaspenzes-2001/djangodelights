ca88f182012793336e3719683639e356c63761d1
tamaspenzes-2001 <penzestamas@proton.me>
2024-08-27 16:40:12 +0200

    modify models

--- inventory/models.py
+++ inventory/models.py
@@ -9,15 +9,27 @@ class Ingredient(models.Model):
   unit_price = models.FloatField()
   name = models.CharField(max_length=30)
 
+  def __str__(self):
+    return f"{self.name} (${self.unit_price}/{self.unit})"
+
 class MenuItem(models.Model):
   price = models.FloatField()
   name = models.CharField(max_length=100)
+
+  def __str__(self):
+    return f"{self.name} (${self.price})"
 
 class Purchase(models.Model):
   menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
   timestamp = models.DateTimeField(default=timezone.now())
 
+  def __str__(self):
+    return f"{self.menu_item.name} purchased at {self.timestamp}"
+
 class RecipeRequirement(models.Model):
   menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
   ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
   quantity = models.FloatField()
+
+  def __str__(self):
+    return f"{self.quantity} {self.ingredient.unit} of {self.ingredient.name} for {self.menu_item.name}"
